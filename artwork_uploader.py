955073b68413bce3447f92adb791021c56ac4751
martinjsteven <martinjsteven@gmail.com>
2025-03-23 14:01:47 +1000

    Test for new autoupdater

--- artwork_uploader.py
+++ artwork_uploader.py
@@ -531,16 +531,15 @@ def setup_web_sockets():
         try:
             # Detect platform
             python_cmd = "python3" if sys.platform == "darwin" else "python"
-            pip_cmd = f"{python_cmd} -m pip"
 
             # Pull latest changes
             subprocess.run(["git", "pull"], check=True)
 
             # Install dependencies
-            subprocess.run([pip_cmd, "install", "-r", "requirements.txt"], check=True, shell=True)
+            subprocess.run([python_cmd, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
 
             # Restart the app
-            os.execlp(python_cmd, python_cmd, "artwork_scraper.py")
+            os.execlp(python_cmd, python_cmd, "artwork_uploader.py")
 
         except Exception as e:
             notify_web(instance,"update_failed", {"error": str(e)})
