f3c808048c4486313eeb99a14e8069ad437ed16c
martinjsteven <martinjsteven@gmail.com>
2025-03-23 17:00:41 +1000

    Test for new autoupdater

--- artwork_uploader.py
+++ artwork_uploader.py
@@ -54,7 +54,7 @@ mode = "cli"
 scheduled_jobs = {}
 scheduled_jobs_by_file = {}
 
-current_version = "0.3.0-beta"
+current_version = "v0.3.0-beta"
 github_repo = "mscodemonkey/artwork-uploader-plex"  # For autoupdater
 
 # ---------------------- CORE FUNCTIONS ----------------------
